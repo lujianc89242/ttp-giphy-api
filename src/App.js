63bec82cc742cacf31544883858507f7d68fa044
Roger Lu <rogerfrontend@gmail.com>
2019-01-10 20:58:22 -0500

    search, render, and env var

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Search from './components/Search';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+        <Search />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/GifCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GifCard(props){
+  return(
+    <ul>{props.data}</ul>
+  );
+}
+
+export default GifCard;
